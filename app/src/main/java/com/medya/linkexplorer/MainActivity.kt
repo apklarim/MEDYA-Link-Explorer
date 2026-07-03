0e0537e5471c90fa3e3eac2177dd660f0d26de8e
apklarim <apklarim@gmail.com>
2026-07-03 22:49:44 +0300

    Add files via upload

--- app/src/main/java/com/medya/linkexplorer/MainActivity.kt
+++ app/src/main/java/com/medya/linkexplorer/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.medya.linkexplorer
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnM3u = findViewById<Button>(R.id.btnM3u)
+
+        btnM3u.setOnClickListener {
+            startActivity(
+                Intent(this, M3uExplorerActivity::class.java)
+            )
+        }
     }
 }
