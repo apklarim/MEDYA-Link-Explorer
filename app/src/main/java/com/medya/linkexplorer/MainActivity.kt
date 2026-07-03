20e624eb03b7a38799349d8a1bf0d98fe7b8dff7
apklarim <apklarim@gmail.com>
2026-07-03 21:26:56 +0300

    Add files via upload

---
+++ app/src/main/java/com/medya/linkexplorer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.medya.linkexplorer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
