3316382844b23f062035b8345643ac3734f1a091
apklarim <apklarim@gmail.com>
2026-07-04 14:42:49 +0300

    Create LinkType.kt

---
+++ app/src/main/java/com/medya/linkexplorer/model/LinkType.kt
@@ -0,0 +1,28 @@
+package com.medya.linkexplorer.model
+
+enum class LinkType {
+
+    HTML,
+
+    M3U,
+
+    M3U8,
+
+    XTREAM,
+
+    MAG,
+
+    JSON,
+
+    XML,
+
+    ZIP,
+
+    IMAGE,
+
+    VIDEO,
+
+    AUDIO,
+
+    UNKNOWN
+}
