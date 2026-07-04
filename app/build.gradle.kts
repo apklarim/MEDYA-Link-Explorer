22bd45ddde8209e897e2ea3b640be0f5d3851581
apklarim <apklarim@gmail.com>
2026-07-04 14:29:26 +0300

    Add dependencies for networking and testing
    Added dependencies for HTTP client, HTML parser, Kotlin coroutines, and AndroidX components.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,11 +13,14 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -40,7 +43,28 @@ android {
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
+
+    // HTTP Client
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    // HTML Parser
+    implementation("org.jsoup:jsoup:1.18.1")
+
+    // Kotlin Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+
+    // AndroidX
+    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.fragment:fragment-ktx:1.8.8")
+
+    // Unit Test
+    testImplementation("junit:junit:4.13.2")
+
+    // Android Test
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
